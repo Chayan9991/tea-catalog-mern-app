4542c5a1d8eb20cf54ca28d3af2e856ca12adc84
Chayan Barman <barmanchayan10@gmail.com>
2024-05-25 11:49:02 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './index.css'
+
+import {Home} from "./category/Home.jsx";
+
+function App() {
+
+    return (
+        <>
+            <Home/>
+        </>
+    )
+}
+
+export default App
